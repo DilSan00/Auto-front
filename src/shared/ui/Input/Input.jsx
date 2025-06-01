3bfe29fd74d6a39d703759c32e0b21d27a710108
DilSan00 <norms777666@gmail.com>
2025-06-01 20:41:06 +0600

    added eye for input passwords

--- src/shared/ui/Input/Input.jsx
+++ src/shared/ui/Input/Input.jsx
@@ -1,8 +1,10 @@
 import clsx from "clsx";
 import s from "./Input.module.scss";
+import { useState } from "react";
+import { IoEyeOffSharp, IoEyeSharp } from "react-icons/io5";
 
 export function Input({
-  type,
+  type = "text",
   placeholder,
   validate,
   label,
@@ -10,14 +12,18 @@ export function Input({
   className,
   ...props
 }) {
+  const [isEye, setIsEye] = useState(false);
+  const isPassword = type === "password";
+  const inputType = isPassword ? (isEye ? "text" : "password") : type;
+
   const handleChange = (e) => {
-    if (onChange) {
-      onChange(e);
-    }
+    onChange?.(e);
   };
 
+  const toggleEye = () => setIsEye((prev) => !prev);
+
   return (
-    <div className={s.input}>
+    <div className={s.inputContainer}>
       <div className={s.container}>
         {label && (
           <label className={s.label} htmlFor={props.id}>
@@ -27,14 +33,22 @@ export function Input({
         {validate && <p className={s.error}>* {validate}</p>}
       </div>
 
-      <input
-        id={props.id}
-        type={type}
-        placeholder={placeholder}
-        onChange={handleChange}
-        className={clsx(s.input, className, type === "number" && s.noSpinner)}
-        {...props}
-      />
+      <div className={s.content}>
+        <input
+          id={props.id}
+          type={inputType}
+          placeholder={placeholder}
+          onChange={handleChange}
+          className={clsx(s.input, className, type === "number" && s.noSpinner)}
+          {...props}
+        />
+
+        {isPassword && (
+          <div className={s.eye} onClick={toggleEye}>
+            {isEye ? <IoEyeSharp size={30} /> : <IoEyeOffSharp size={30} />}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
